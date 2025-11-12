7f3cae55fc0e5b25d3045e818c7e1ba211b6ebff
mogmoog <73155376+mogmoo123@users.noreply.github.com>
2025-11-12 11:53:21 +0900

    Add files via upload

--- OCTA.py
+++ OCTA.py
@@ -26,6 +26,7 @@ class completeAlart():
         self.defualt_path = str(Path(data['defualt_path']))+"/"
         self.error_msg = ""
         self.current = ""
+        self.terminal_time = ""
         self.time = 0
         self.RUKA_icon = {
             'src': str(Path(self.icon).resolve()),
@@ -35,12 +36,14 @@ class completeAlart():
     def start(self):
         while True:
             self.process()
-            sleep(1)
 
     def process(self):
         path = self.find()
-        if is_initial(self.current,self.time): #체크
+        initial = is_initial(path)
+        #print("initial : ",initial)
+        if initial: #체크
             self.check(path)
+
 
     def find(self):
         file = glob(self.defualt_path+'*.out')
@@ -54,69 +57,92 @@ class completeAlart():
 
     def check(self,arg):
         path = arg
-        checkin(self.current, self.time)
         if not os.path.exists(path):
             self.notfound_alart()
         try:
-            with open(path, "r", encoding='UTF-8') as f:
-                for line in f:
-                    if "****ORCA TERMINATED NORMALLY****" in line:
-                        #체크
-                        self.complete_alart()
-                        return True
-                    elif "!!!" in line:
-                        self.error_msg = f"ORCA 오류 발생, {self.current} 파일을 확인 바랍니다."
-                        self.fail_alart()
-                        return False
+            try:
+                with open(path, "r", encoding='UTF-8') as f:
+                    for line in f:
+                        if "****ORCA TERMINATED NORMALLY****" in line:
+                            #체크'
+                            self.terminal_time = f.readline()[16:49]
+                            self.complete_alart()
+                            checkin(path)
+                            return True
+                        elif "!!!     ERROR (ORCA_MAIN):" in line:
+                            self.terminal_time = f.readline()[16:49]
+                            self.error_msg = f"ORCA 오류 발생, {self.current} 파일을 확인 바랍니다."
+                            self.fail_alart()
+                            checkin(path)
+                            return False
+            except UnicodeDecodeError:
+                with open(path, "r", encoding='UTF-16') as f:
+                    for line in f:
+                        if "****ORCA TERMINATED NORMALLY****" in line:
+                            #체크
+                            self.terminal_time = f.readline()[16:49]
+                            self.complete_alart()
+                            checkin(path)
+                            return True
+                        elif "!!!     ERROR (ORCA_MAIN):" in line:
+                            self.terminal_time = f.readline()[16:49]
+                            self.error_msg = f"ORCA 오류 발생, {self.current} 파일을 확인 바랍니다."
+                            self.fail_alart()
+                            checkin(path)
+                            return False
         except Exception as e:
             self.error_msg = str(e)
             self.fail_alart()
+            checkin(path)
             return False
 
     def complete_alart(self):
         if self.complete_sound == "None":
             toast("Success",
-                  "ORCA 계산이 완료되었습니다.",
+                  f"ORCA 계산이 완료되었습니다. \n {self.terminal_time}",
                   icon=self.RUKA_icon,
                   scenario='incomingCall'
                   )
         else:
             voice = str(Path(self.complete_sound).resolve())
             toast("Success",
-                  "ORCA 계산이 완료되었습니다.",
+                  f"{self.current}\nORCA 계산이 완료되었습니다. \n {self.terminal_time}",
                   icon=self.RUKA_icon,
                   audio=voice,
                   scenario='incomingCall'
                   )
+        sleep(1)
 
 
     def notfound_alart(self):
         if self.notfound_sound == "None":
             toast("Unkown File",
-                  "파일을 찾을 수 없습니다.",
+                  f"파일을 찾을 수 없습니다. \n {self.terminal_time}",
                   icon=self.RUKA_icon,
                   scenario='incomingCall'
                   )
         else:
             voice = str(Path(self.notfound_sound).resolve())
             toast("Unkown File",
-                  "파일을 찾을 수 없습니다.",
+                  f"파일을 찾을 수 없습니다. \n {self.terminal_time}",
                   icon=self.RUKA_icon,
                   audio=voice,
                   scenario='incomingCall'
                   )
+        sleep(1)
 
 
     def fail_alart(self):
         if self.fail_sound == "None":
-            toast("Fail", f"{self.error_msg}",
+            toast("Fail", f"{self.error_msg} \n {self.terminal_time}",
                   icon=self.RUKA_icon,
                   scenario='incomingCall'
                   )
         else:
             voice = str(Path(self.fail_sound).resolve())
-            toast("Fail", f"{self.error_msg}",
+            toast("Fail", f"{self.error_msg} \n {self.terminal_time}",
                   icon=self.RUKA_icon,
                   audio=voice,
                   scenario='incomingCall'
                   )
+        sleep(1)
